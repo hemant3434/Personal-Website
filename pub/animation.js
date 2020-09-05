47c24a6073dcf52ac895fdd87c3905096c343c70
Hemant Bhanot <hemant.bhanot01@gmail.com>
2020-09-05 03:46:40 -0400

    completed projects

---
+++ pub/animation.js
@@ -0,0 +1,29 @@
+"use strict";
+const log = console.log;
+
+let myIndex = [0, 0, 0, 0, 0];
+const CYCLE_TIME = 5000;
+carousel();
+
+function carousel() {
+  animate("queue", 0);
+  animate("spotify", 1);
+  animate("alarm", 2);
+  animate("sense", 3);
+  animate("style", 4);
+
+  setTimeout(carousel, CYCLE_TIME);
+}
+
+function animate(className, index) {
+  let i;
+  const x = document.getElementsByClassName(className);
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  myIndex[index]++;
+  if (myIndex[index] > x.length) {
+    myIndex[index] = 1;
+  }
+  x[myIndex[index] - 1].style.display = "block";
+}
